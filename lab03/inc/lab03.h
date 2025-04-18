8504c8043fc285df1e29d4f4daf8392864d3652d
Home WSL Diogo <hiperdg.gm@gmail.com>
2025-04-18 10:48:32 +0100

    lib update

--- lab03/inc/lab03.h
+++ lab03/inc/lab03.h
@@ -27,8 +27,8 @@ typedef struct algos
 } t_algos;
 
 /* ----- Modifiable Macros ----- */
-#  define ORDER 2 // 0 for ascending, 1 for descending, 2 for random
-#  define SIZE 100 // size of the array
+#  define ORDER 2	// 0 for ascending, 1 for descending, 2 for random
+#  define SIZE 100	// size of the array
 
 /* ----- Non-Modfiable Macros ----- */
 
